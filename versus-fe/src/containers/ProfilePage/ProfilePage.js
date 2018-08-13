9b7299b1c70e95d48ac92fd16f007f1f340725de
OliWalker <ofwalker@live.co.uk>
2018-08-13 12:35:22 +0200

    second commit

---
+++ versus-fe/src/containers/ProfilePage/ProfilePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './ProfilePage.css';
+
+export default class ProfilePage extends Component {
+  render() {
+    return (
+      <div className="ProfilePage">
+        <div className="ProfilePage picture">
+          <img src="" />
+        </div>
+
+        <div className="ProfilePage stats">
+          
+        </div>
+
+      </div>
+    )
+  }
+}
