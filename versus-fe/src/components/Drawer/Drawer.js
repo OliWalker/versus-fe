667199311aeeb7e07b69466c2f5122deeab75892
OliWalker <ofwalker@live.co.uk>
2018-08-13 23:12:33 +0200

    profilePage lookin' sweet

--- versus-fe/src/components/Drawer/Drawer.js
+++ versus-fe/src/components/Drawer/Drawer.js
@@ -1,15 +1,58 @@
 import React from 'react'
 import './Drawer.css'
+import { Link } from 'react-router-dom'
 
-const cheese = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+const toggleLeagues = () => {
+  const LeagueList = document.querySelector('.Drawer__LeagueList')
+  const LeagueListSport = document.querySelectorAll('.Drawer__LeagueList__sport')
 
 
+  if (LeagueList.classList.contains('Drawer__LeagueList__closed')) {
+    LeagueList.classList.remove('Drawer__LeagueList__closed')
+    LeagueListSport.forEach(el => el.classList.remove('Drawer__LeagueList__closed', 'text'))
+  } else {
+    LeagueList.classList.add('Drawer__LeagueList__closed')
+    LeagueListSport.forEach(el => el.classList.add('Drawer__LeagueList__closed', 'text'))
+  }
+}
 
 export default function Drawer (props) {
   return (
-    <div className='Drawer'> 
-      <h2> drawer </h2>
-      {cheese.map(el => <span className='Drawer__link'> link to somewhere </span>)}
+    <div className="Drawer">
+      <div className="Drawer__logo">
+        <h1> Versus </h1>
+      </div> 
+
+      <div className="Drawer__button" onClick={toggleLeagues}>
+        <i className="fas fa-trophy"></i>
+        <span> Leagues </span>
+      </div>
+
+      <div className="Drawer__LeagueList Drawer__LeagueList__closed">
+        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Tennis </span>
+        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Eating </span>
+        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Ping-Pong </span>
+        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Juggling </span>
+        {/* <span> Flying </span> */}
+      </div>
+
+      <div className="Drawer__button">
+        <i className="far fa-calendar-check"></i>
+        <span> Matches</span>
+      </div>
+
+      
+      <div className="Drawer__button">
+        <i className="fas fa-futbol"></i>
+        <span>Sports</span>
+      </div>
+    
+
+      <div className="Drawer__button">
+        <i className="fas fa-power-off"></i>
+        <span> Log-out </span>
+      </div>
+
     </div>
   )
 }
--- versus-fe/src/containers/ProfilePage/ProfilePage.js
+++ versus-fe/src/containers/ProfilePage/ProfilePage.js
@@ -2,16 +2,55 @@ import React, { Component } from 'react';
 import './ProfilePage.css';
 
 export default class ProfilePage extends Component {
+
+  componentDidMount() {
+    fetch('http://private-1cf21-versus3.apiary-mock.com/user/1')
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
+
+
   render() {
     return (
 
       <div className="ProfilePage">
+
         <div className="ProfilePage__picture">
-          <img src="" />
+          <img alt="random Dude" src="http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-picture-black-and-white-1.jpg" />
         </div>
 
+        <span className="ProfilePage__name"> Chad Jenkins </span>
+
+        <span className="ProfilePage__score"><i>1820</i></span>
+
+        <div className="ProfilePage__all__scores"> 
+
+          <div className="ProfilePage__sport__score">
+            <span className="ProfilePage__sport__score__single"> <i>1000</i> </span>
+            <span className="ProfilePage__sport__name__single"> Tennis </span>
+          </div>
+          <div className="ProfilePage__sport__score">
+            <span className="ProfilePage__sport__score__single"> <i>600</i> </span>
+            <span className="ProfilePage__sport__name__single"> Eating </span>
+          </div>
+          <div className="ProfilePage__sport__score">
+            <span className="ProfilePage__sport__score__single"> <i>1900</i> </span>
+            <span className="ProfilePage__sport__name__single"> Ping-Pong </span>
+          </div>
+          <div className="ProfilePage__sport__score">
+            <span className="ProfilePage__sport__score__single"> <i>2000</i> </span>
+            <span className="ProfilePage__sport__name__single"> Juggling </span>
+          </div>
+          <div className="ProfilePage__sport__score">
+            <span className="ProfilePage__sport__score__single"> <i>1200</i> </span>
+            <span className="ProfilePage__sport__name__single"> Flying </span>
+          </div>
+
+        </div>
+
+
         <div className="ProfilePage__stats">
-          
+          <img src="./graph.png" />
         </div>
 
       </div>
