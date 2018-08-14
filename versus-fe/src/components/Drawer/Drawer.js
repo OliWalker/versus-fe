17bc8b2e99ee43cd4bc0f80c46773125a66a3b18
OliWalker <ofwalker@live.co.uk>
2018-08-14 10:31:13 +0200

    league table touchup

--- versus-fe/src/components/Drawer/Drawer.js
+++ versus-fe/src/components/Drawer/Drawer.js
@@ -23,34 +23,38 @@ export default function Drawer (props) {
         <h1> Versus </h1>
       </div> 
 
-      <div className="Drawer__button" onClick={toggleLeagues}>
-        <i className="fas fa-trophy"></i>
-        <span> Leagues </span>
-      </div>
+      <div className="Drawer__button__list">
 
-      <div className="Drawer__LeagueList Drawer__LeagueList__closed">
-        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Tennis </span>
-        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Eating </span>
-        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Ping-Pong </span>
-        <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Juggling </span>
-        {/* <span> Flying </span> */}
-      </div>
+        <div className="Drawer__button" onClick={toggleLeagues}>
+          <i className="fas fa-trophy"></i>
+          <span> Leagues </span>
+        </div>
 
-      <div className="Drawer__button">
-        <i className="far fa-calendar-check"></i>
-        <span> Matches</span>
-      </div>
+        <div className="Drawer__LeagueList Drawer__LeagueList__closed">
+          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Tennis </span>
+          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Eating </span>
+          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Ping-Pong </span>
+          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Juggling </span>
+          {/* <span> Flying </span> */}
+        </div>
 
+        <div className="Drawer__button">
+          <i className="far fa-calendar-check"></i>
+          <span> Matches</span>
+        </div>
+
+        
+        <div className="Drawer__button">
+          <i className="fas fa-futbol"></i>
+          <span>Sports</span>
+        </div>
       
-      <div className="Drawer__button">
-        <i className="fas fa-futbol"></i>
-        <span>Sports</span>
-      </div>
-    
 
-      <div className="Drawer__button">
-        <i className="fas fa-power-off"></i>
-        <span> Log-out </span>
+        <div className="Drawer__button">
+          <i className="fas fa-power-off"></i>
+          <span> Log-out </span>
+        </div>
+
       </div>
 
     </div>
