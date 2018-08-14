c71fe522a208de056ef0030e22a2892fa0c664c2
OliWalker <ofwalker@live.co.uk>
2018-08-14 19:14:38 +0200

    more redux

--- versus-fe/src/components/Drawer/Drawer.js
+++ versus-fe/src/components/Drawer/Drawer.js
@@ -1,68 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './Drawer.css'
 import { Link } from 'react-router-dom'
+
 
 const toggleLeagues = () => {
   const LeagueList = document.querySelector('.Drawer__LeagueList')
   const LeagueListSport = document.querySelectorAll('.Drawer__LeagueList__sport')
 
 
-  if (LeagueList.classList.contains('Drawer__LeagueList__closed')) {
+  if (LeagueListSport[0].classList.contains('Drawer__LeagueList__closed')) {
     LeagueList.classList.remove('Drawer__LeagueList__closed')
     LeagueListSport.forEach(el => el.classList.remove('Drawer__LeagueList__closed', 'text'))
   } else {
-    LeagueList.classList.add('Drawer__LeagueList__closed')
+    // LeagueList.classList.add('Drawer__LeagueList__closed')
     LeagueListSport.forEach(el => el.classList.add('Drawer__LeagueList__closed', 'text'))
   }
 }
 
-export default function Drawer (props) {
+class Drawer extends Component {
 
-  return (
-    <div className="Drawer">
+  constructor(props) {
+    super(props)
+  }
 
-      <Link to='/profile'>
-        <div className="Drawer__logo">
-          <h1> Versus </h1>
-        </div> 
-      </Link>
+  render () {
 
-      <div className="Drawer__button__list">
+    const stats = this.props.stats
+    // if (this.props.matches[0]) const notifications = this.props.matches.filter(match => match.status === 'pending').length
 
-          <div className="Drawer__button" onClick={toggleLeagues}>
-            <i className="fas fa-trophy"></i>
-            <span> Leagues </span>
+    return (
+      <div className="Drawer">
+
+        <Link to='/profile'>
+          <div className="Drawer__logo">
+            <h1> Versus </h1>
+          </div> 
+        </Link>
+
+        <div className="Drawer__button__list">
+
+            <div className="Drawer__button" onClick={toggleLeagues}>
+              <i className="fas fa-trophy"></i>
+              <span> Leagues </span>
+            </div>
+
+          <div className="Drawer__LeagueList ">
+
+            {stats[0] ? 
+            stats.map(league => {
+              return ( <Link to={`/league/${league.league_id}`}
+                className="Drawer__LeagueList__sport Drawer__LeagueList__closed">
+                {league.name}
+                </Link>
+              )
+            }) : null }
+
           </div>
 
-        <div className="Drawer__LeagueList Drawer__LeagueList__closed">
-          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Tennis </span>
-          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Eating </span>
-          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Ping-Pong </span>
-          <span className="Drawer__LeagueList__sport Drawer__LeagueList__closed text"> Juggling </span>
+          <Link to='/matches'>
+            <div className="Drawer__button">
+              <i className="far fa-calendar-check"></i>
+              <span> Matches</span>
+            </div>
+          </Link>
+
+          <Link to='/sportsList'>
+            <div className="Drawer__button">
+              <i className="fas fa-futbol"></i>
+              <span>Sports</span>
+            </div>
+          </Link>
+        
+          <Link to='/'>
+            <div className="Drawer__button">
+              <i className="fas fa-power-off"></i>
+              <span> Log-out </span>
+            </div>
+          </Link>
         </div>
 
-        <Link to='/matches'>
-          <div className="Drawer__button">
-            <i className="far fa-calendar-check"></i>
-            <span> Matches</span>
-          </div>
-        </Link>
-
-        <Link to='/sportsList'>
-          <div className="Drawer__button">
-            <i className="fas fa-futbol"></i>
-            <span>Sports</span>
-          </div>
-        </Link>
-      
-        <Link to='/'>
-          <div className="Drawer__button">
-            <i className="fas fa-power-off"></i>
-            <span> Log-out </span>
-          </div>
-        </Link>
       </div>
-
-    </div>
-  )
+    )
+  }
 }
+
+const mapStateToProps = (state) => ({
+  stats: state.stats,
+  matches: state.matches
+})
+
+
+export default connect(mapStateToProps)(Drawer)
