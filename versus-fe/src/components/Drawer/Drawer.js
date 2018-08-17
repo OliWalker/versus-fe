b3e8475a25ee829022fd621ba8092dc7bfe5c673
OliWalker <ofwalker@live.co.uk>
2018-08-17 22:03:50 +0200

    drawer working goood

--- versus-fe/src/components/Drawer/Drawer.js
+++ versus-fe/src/components/Drawer/Drawer.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Drawer.css';
-
 import { Link } from 'react-router-dom';
+import { TweenMax } from 'gsap';
 
 class Drawer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerClass: '',
-      hamburgerClass: '',
-      leaguesClass: 'Drawer__LeagueList__closed'
+      drawerStatus: '',
+      hamburgerStatus: '',
+      leaguesStatus: ''
     };
   }
 
   toggleLeagues = () => {
-    const leaguesClass =
-      this.state.leaguesClass === '' ? 'Drawer__LeagueList__closed' : '';
-    this.setState({ leaguesClass });
+    const target = document.querySelector('.Drawer__LeagueList');
+    if (this.state.leaguesStatus === '') {
+      TweenMax.to(target, 0.2, {
+        height: 300
+      });
+      this.setState({ leaguesStatus: 'open' });
+    } else {
+      TweenMax.to(target, 0.2, {
+        height: 0
+      });
+      this.setState({ leaguesStatus: '' });
+    }
   };
 
   openDrawer = () => {
-    const hamburgerClass =
-      this.state.hamburgerClass === '' ? 'Header__hamburger__open' : '';
-    const drawerClass =
-      this.state.drawerClass === '' ? 'Header__drawer__open' : '';
-    this.setState({ hamburgerClass, drawerClass });
+    const hamburgerStatus =
+      this.state.hamburgerStatus === '' ? 'Drawer__hamburger__open' : '';
+    const drawerStatus =
+      this.state.drawerStatus === '' ? 'Drawer__drawer__open' : '';
+    this.setState({ hamburgerStatus, drawerStatus });
   };
 
   render() {
@@ -36,74 +45,65 @@ class Drawer extends Component {
     ).length;
 
     return (
-      <div className="Header">
-        <div className={`Header__drawer ${this.state.drawerClass}`}>
-          <div className="Drawer">
-            <Link to="/profile">
-              <div className="Drawer__logo">
-                <h1> Versus </h1>
-              </div>
-            </Link>
-
-            <div className="Drawer__button__list">
-              <div className="Drawer__button" onClick={this.toggleLeagues}>
-                <i className="fas fa-trophy" />
-                <span> Leagues </span>
-              </div>
-
-              <div className="Drawer__LeagueList ">
-                {stats[0]
-                  ? stats.map(league => {
-                      return (
-                        <Link
-                          key={league.league_id}
-                          to={`/league/${league.league_id}`}
-                          className={`Drawer__LeagueList__sport ${
-                            this.state.leaguesClass
-                          }`}
-                        >
-                          {league.name}
-                        </Link>
-                      );
-                    })
-                  : null}
-              </div>
-
-              <Link to="/myMatches">
-                <div className="Drawer__button Drawer__button__notie">
-                  {notifications > 0 ? (
-                    <div className="Drawer__button__notification">
-                      {notifications}
-                    </div>
-                  ) : null}
-                  <i className="far fa-calendar-check" />
-                  <span> Matches</span>
-                </div>
-              </Link>
-
-              <Link to="/sportsList">
-                <div className="Drawer__button">
-                  <i className="fas fa-futbol" />
-                  <span>Sports</span>
-                </div>
-              </Link>
-
-              <Link to="/">
-                <div className="Drawer__button">
-                  <i className="fas fa-power-off" />
-                  <span> Log-out </span>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-
+      <div>
         <i
-          className={`fas fa-bars Header__hamburger ${
-            this.state.hamburgerClass
+          className={`fas fa-bars Drawer__hamburger ${
+            this.state.hamburgerStatus
           }`}
           onClick={this.openDrawer}
         />
+        <div className={`Drawer ${this.state.drawerStatus}`}>
+          <Link to="/profile">
+            <h1 className="Drawer__logo">Versus</h1>
+          </Link>
+
+          <div className="Drawer__button__list">
+            <div className="Drawer__button" onClick={this.toggleLeagues}>
+              <i className="fas fa-trophy" />
+              <span> Leagues </span>
+            </div>
+
+            <div className={'Drawer__LeagueList'}>
+              {stats.map(league => {
+                return (
+                  <Link
+                    key={league.league_id}
+                    to={`/league/${league.league_id}`}
+                    className={'Drawer__LeagueList__sport'}
+                  >
+                    {league.name}
+                  </Link>
+                );
+              })}
+            </div>
+
+            <Link to="/myMatches">
+              <div className="Drawer__button Drawer__button__notie">
+                {notifications > 0 ? (
+                  <div className="Drawer__button__notification">
+                    {notifications}
+                  </div>
+                ) : null}
+                <i className="far fa-calendar-check" />
+                <span> Matches</span>
+              </div>
+            </Link>
+
+            <Link to="/sportsList">
+              <div className="Drawer__button">
+                <i className="fas fa-futbol" />
+                <span>Sports</span>
+              </div>
+            </Link>
+
+            <Link to="/">
+              <div className="Drawer__button">
+                <i className="fas fa-power-off" />
+                <span> Log-out </span>
+              </div>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
