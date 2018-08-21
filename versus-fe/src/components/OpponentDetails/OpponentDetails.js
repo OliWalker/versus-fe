58af746ec9cd87dd243d0c60e25d9387745a4dc6
OliWalker <ofwalker@live.co.uk>
2018-08-21 20:08:59 +0200

    toggling buttons

--- versus-fe/src/components/OpponentDetails/OpponentDetails.js
+++ versus-fe/src/components/OpponentDetails/OpponentDetails.js
@@ -3,11 +3,19 @@ import PastMatchCard from '../PastMatchCard/PastMatchCard';
 import './OpponentDetails.css';
 
 class OpponentDetails extends Component {
+  state = {
+    buttonClass: 'Match History'
+  };
+
   renderMatch_history = () => {
     const previousMatches = this.props.theOpponent.match_history;
     return previousMatches.map(previousMatch => (
       <PastMatchCard pastMatch={previousMatch} />
     ));
+  };
+
+  buttonClass = buttonName => {
+    return this.state.buttonClass === buttonName ? 'active' : null;
   };
 
   render() {
@@ -22,8 +30,14 @@ class OpponentDetails extends Component {
           {this.props.theOpponent.username}
         </div>
         <div className="Opponent__Details__buttons">
-          <button> Match History </button>
-          <button> Performance </button>
+          <button className={() => this.buttonClass('Match History')}>
+            {' '}
+            Match History{' '}
+          </button>
+          <button className={() => this.buttonClass('Performance')}>
+            {' '}
+            Performance{' '}
+          </button>
         </div>
         <div className="opponentPastGames">{this.renderMatch_history()}</div>
       </div>
