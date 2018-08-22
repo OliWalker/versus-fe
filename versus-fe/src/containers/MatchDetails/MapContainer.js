88e8e6783c9e5166dcf749219999c6eb9a013eb7
Jamie.Y <carvej.18@gmail.com>
2018-08-22 23:46:59 +0200

    marker relocates on map based on entry

--- versus-fe/src/containers/MatchDetails/MapContainer.js
+++ versus-fe/src/containers/MatchDetails/MapContainer.js
@@ -2,38 +2,45 @@ import React, { Component } from 'react'
 import {Map , InfoWindow , Marker , GoogleApiWrapper } from 'google-maps-react';
 
 import { connect } from 'react-redux'
+import { requestLocation } from '../../redux/actions'
 
 export class MapContainer extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      markerPosition: {}
+    }
+  }
+
   queryPlaces = () => {
-        const params = {
-          key: 'AIzaSyBzBvfaosQJN9iUMMRAPD9ATnIPjofrCto',
-          input: 'Nova Icaria',
-          inputtype: 'textquery',
-          fields: 'formatted_address,name,opening_hours,geometry'
-        }
 
-        const paramToString = (obj1) => {
-          let aStr = ''
-          for(let key in obj1) {
-            const keyString = obj1[key].split(' ').join('\%20')
-            aStr = aStr.concat(key.toString(), '=', keyString, '&')
-          }
-          return aStr.slice(0, (aStr.length-1));
-        }
+    const params = {
+      input: this.props.location
+    }
 
-        const paramStr = paramToString(params)
-
+    this.props.requestLocation({
+      endpoint: '/requestLocation',
+      method:'POST',
+      body: params
+    })
 
   }
 
   componentDidUpdate(prevProps) {
-    if(this.props.location !== prevProps.location){
+    if (this.props.location !== prevProps.location) {
       this.queryPlaces()
     }
-
+    if (this.props.googlePlaces !== prevProps.googlePlaces) {
+      this.setState({
+        markerPosition: this.props.googlePlaces.candidates[0].geometry.location
+      })
+    }
   }
 
+  renderMarker = () => {
+    return <Marker position={this.state.markerPosition}/>
+  }
   render(){
     const style = {
       width: '100%',
@@ -43,26 +50,38 @@ export class MapContainer extends Component {
       height: '50%'
     }
     return(
-      <Map google={this.props.google} style={style} containerStyle={containerStyle} zoom={14}>
+      <Map
+        google={this.props.google}
+        style={style}
+        initialCenter={{
+          lat: 41.379327,
+          lng: 2.173349
+        }}
+        containerStyle={containerStyle}
+        zoom={14}
+      >
 
-        <Marker onClick={this.onMarkerClick}
-            name={'Match Location'} />
+      {this.renderMarker()}
 
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
             <h1> </h1>
           </div>
         </InfoWindow>
+
       </Map>
       )
     }
   }
 
 const mapStateToProps = state => ({
-  location: state.location
+  location: state.location,
+  googlePlaces: state.googlePlaces
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+  requestLocation: (apiInfo) =>  dispatch(requestLocation(apiInfo))
+})
 
   export default connect(
     mapStateToProps,
