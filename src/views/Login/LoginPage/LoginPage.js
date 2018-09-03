c6e514c87925348377819169139e495618a6e31b
OliWalker <ofwalker@live.co.uk>
2018-09-03 17:34:57 +0200

    setup for demo

--- src/views/Login/LoginPage/LoginPage.js
+++ src/views/Login/LoginPage/LoginPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './LoginPage.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logIn } from '../../../redux/actions';
+import { logIn, getUserInfo } from '../../../redux/actions';
 
 export class LoginPage extends Component {
   constructor(props) {
@@ -29,9 +29,10 @@ export class LoginPage extends Component {
   };
 
   sendDetails = event => {
-    const details = btoa(this.state.username + ':' + this.state.password);
-    const loginDetails = { Authorization: `Basic ${details}` };
-    this.props.logIn(loginDetails);
+    // const details = btoa(this.state.username + ':' + this.state.password);
+    // const loginDetails = { Authorization: `Basic ${details}` };
+    // this.props.logIn(loginDetails);
+    this.props.getUserInfo(1);
   };
 
   handleFormChange = event => {
@@ -44,20 +45,20 @@ export class LoginPage extends Component {
     const loginClass = this.state.loginError ? 'Error' : '';
     return (
       <div className="loginContainer">
-        <img className="logo" src="backgrounds/logo.png" alt="App logo" />
+        <img className="logo" src="/backgrounds/logo.png" alt="App logo" />
 
         <div className={`loginDetails ${loginClass}`}>
           <form>
             <input
               type="text"
-              value={this.state.username}
+              value="Guest!"
               onChange={this.handleFormChange}
               name="username"
               placeholder="Username"
             />
             <input
               type="password"
-              value={this.state.password}
+              value=""
               onChange={this.handleFormChange}
               name="password"
               placeholder="Password"
@@ -67,14 +68,13 @@ export class LoginPage extends Component {
 
         <div className="organize">
           <div className="submit" onClick={this.sendDetails}>
-            Sign In
+            Demo The App
           </div>
 
           <div className="SignUp">
-            <Link to="/SignUp">
-              {' '}
-              New user? <strong> Sign up. </strong>{' '}
-            </Link>
+            {/* <Link to="/SignUp">
+              New user? <strong> Sign up. </strong>
+            </Link> */}
           </div>
         </div>
       </div>
@@ -87,7 +87,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  logIn: details => dispatch(logIn({ endpoint: '/login', headers: details }))
+  logIn: details => dispatch(logIn({ endpoint: '/login', headers: details })),
+  getUserInfo: user_id =>
+    dispatch(getUserInfo({ endpoint: `/users/${user_id}` }))
 });
 
 export default connect(
